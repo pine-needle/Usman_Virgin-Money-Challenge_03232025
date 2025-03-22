256b47a0f23dd20afc826b96f611d14e7e43fe28
Asante Adarkwa Usman <usmanadarkwa@gmail.com>
2025-03-22 18:02:11 -0400

    Configured project with multi-module structure, google font added in the core module

---
+++ core/src/main/java/com/example/core/theme/Color.kt
@@ -0,0 +1,8 @@
+package com.example.core.theme
+
+import androidx.compose.ui.graphics.Color
+
+val PrimaryColor = Color(0xFFC40202)
+val SecondaryColor = Color(0xFFA6AAB4)
+val TertiaryColor = Color(0xFF017DFF)
+val BackgroundWhite = Color(0xFFF9FAFB)
---
+++ settings.gradle.kts
@@ -0,0 +1,28 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Virgin Money"
+include(":app")
+include(":core")
+include(":data")
+include(":domain")
+include(":feature_people")
+include(":feature_rooms")
